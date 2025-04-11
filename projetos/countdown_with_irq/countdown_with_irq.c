c2943d3ff9de40ea965277f1ee82cda51319edaf
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-04-11 15:17:26 -0300

    Contador decrescente finalizado

--- projetos/countdown_with_irq/countdown_with_irq.c
+++ projetos/countdown_with_irq/countdown_with_irq.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
@@ -9,6 +10,14 @@
 #define I2C_SDA 14
 #define I2C_SCL 15
 
+volatile bool reset = true;
+
+void btn_a_callback(uint gpio, uint32_t events)
+{
+    if (gpio == BTN_A && (events & GPIO_IRQ_EDGE_FALL))
+        reset = true;
+}
+
 int main()
 {
     stdio_init_all();
@@ -17,6 +26,7 @@ int main()
     gpio_init(BTN_A);
     gpio_set_dir(BTN_A, GPIO_IN);
     gpio_pull_up(BTN_A);
+    gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_FALL, true, &btn_a_callback);
 
     // Botão B
     gpio_init(BTN_B);
@@ -32,10 +42,10 @@ int main()
     ssd1306_init();
 
     struct render_area frame = {
-        start_column: 0,
-        end_column: ssd1306_width - 1,
-        start_page: 0,
-        end_page: ssd1306_n_pages - 1,
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1,
     };
 
     calculate_render_area_buffer_length(&frame);
@@ -58,23 +68,63 @@ int main()
     };
 
     int x_pos = 64; // Centraliza o texto horizontalmente na tela
-    int y_pos = 32; // Centraliza o texto verticalmente na tela
+    int y_pos = 16; // Centraliza o texto verticalmente na tela
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    int x2_pos = x_pos - 48;
+    int y2_pos = y_pos + 16;
 
-        const size_t msg_len = sizeof(msg) / sizeof(msg[0]);
-        for (size_t i = 0; i < msg_len; i++) {
+    bool last_btn_b_state = true;
+    char str_buffer[16];
 
-            ssd1306_draw_string(ssd, x_pos, y_pos, msg[i]);
-            render_on_display(ssd, &frame);
-            sleep_ms(1000);
+    while (true)
+    {
+        if (reset)
+        {
+            reset_loop:
+        
+            reset = false;
+            
+            const size_t msg_len = sizeof(msg) / sizeof(msg[0]);
+            uint16_t count_btn_b = 0;
+
+            for (size_t i = 0; i < msg_len; i++)
+            {
+                memset(ssd, 0, ssd1306_buffer_length);
+                ssd1306_draw_string(ssd, x_pos, y_pos, msg[i]);
+                sprintf(str_buffer, "Button b: %d", count_btn_b);
+                ssd1306_draw_string(ssd, x2_pos, y2_pos, str_buffer);
+                render_on_display(ssd, &frame);
+
+                uint32_t start_time = to_ms_since_boot(get_absolute_time());
+                while (to_ms_since_boot(get_absolute_time()) - start_time < 1000)
+                {
+                    bool btn_b_state = gpio_get(BTN_B);
+
+                    if (!btn_b_state && last_btn_b_state)
+                    {
+                        count_btn_b++;
+                        memset(ssd, 0, ssd1306_buffer_length);
+                        ssd1306_draw_string(ssd, x_pos, y_pos, msg[i]);
+                        sprintf(str_buffer, "Button b: %d", count_btn_b);
+                        ssd1306_draw_string(ssd, x2_pos, y2_pos, str_buffer);
+                        render_on_display(ssd, &frame);
+                    }
+                    last_btn_b_state = btn_b_state;
+
+                    if (!gpio_get(BTN_A)) {
+                        sleep_ms(50);
+                        goto reset_loop;
+                    }
+
+                    sleep_ms(10);
+                }
+
+                sprintf(str_buffer, "Button b: %d", count_btn_b);
+                ssd1306_draw_string(ssd, x2_pos, y2_pos, str_buffer);
+                render_on_display(ssd, &frame);
+            }
         }
 
-        ssd1306_draw_string(ssd, 0, y_pos, "End of countdown"); 
-        render_on_display(ssd, &frame);
-        sleep_ms(100);
-        return 0;
+        tight_loop_contents();
     }
 }
